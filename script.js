e8574e9b861194a2b81dc678e8672e32cb0b83b8
kmns1 <kmnse01@gmail.com>
2026-08-18 21:34:15 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
- function setVH() {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
- }
-
-window.addEventListener('resize', setVH);
-setVH();
-
 const radio = document.querySelector('.radio')
 const background = document.querySelector('.background')
 const volume = document.querySelectorAll('.volumeSet')
